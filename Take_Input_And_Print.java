e3bb18ae8bb30367955f5f66d89f979ecdf27477
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2023-08-19 12:13:40 +0530

    import java.util.*; public class sum_array {     public static void main(String[] args) {         Scanner sc = new Scanner(System.in);         String n = sc.nextLine();         System.out.print(n);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sum_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        System.out.print(n);
+       
+    }
+}
