a1434b3367b0aa30f17fef6077337e74e28421b3
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2023-08-25 11:54:26 +0530

    import java.util.*; public class Satya{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         for(int i = 0; i<t; i++){             int x = sc.nextInt();             int y = sc.nextInt();             System.out.println(x/y+x%y);         }     } }

---
+++ Yet_another_climbing_problem.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Satya{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i<t; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x/y+x%y);
+        }
+    }
+}
