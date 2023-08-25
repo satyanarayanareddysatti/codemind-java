98482c5ecde08b3d42bf5ea094daa129d43744dc
satyanarayanareddysatti <119919492+satyanarayanareddysatti@users.noreply.github.com>
2023-08-25 12:11:03 +0530

    import java.util.*; public class Satya{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String n = sc.next();         System.out.print(Integer.parseInt(n, 8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Satya{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        System.out.print(Integer.parseInt(n, 8));
+    }
+}
